5643bdd587b874c729a02a539353801e3168bd9c
Sangmoo <first_1st@naver.com>
2021-12-27 01:08:58 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 function App() {
   const [counter, setVal] = useState(0);
   const onClick = () => setVal((prev) => prev + 1)
-  console.log("Call Api")
+  
+  console.log("Run All Time")
+
+  useEffect(() => {
+    console.log("Call Api") // Api를 한번만 호출 - useEffect
+  }, []) // [] 안에 counter가 들어갈시 useState가 변경될 때마다 리렌더링, []가 비어있으면 한번만 실행, 부분적으로 한번만 실행시킬때 유용
   return (
     <div>
       <h1>{counter}</h1>
