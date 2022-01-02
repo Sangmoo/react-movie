c5b9adbf6fadf32b4a2aefab856a99cdb9aec4cd
Sangmoo <first_1st@naver.com>
2022-01-02 23:18:20 +0900

    prac react

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import { useState, useEffect } from "react"
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Detail from './routes/Detail';
+import Home from './routes/Home';
 function App() {
-  const [counter, setVal] = useState(0);
-  const onClick = () => setVal((prev) => prev + 1)
-  
-  console.log("Run All Time")
-
-  useEffect(() => {
-    console.log("Call Api") // Api를 한번만 호출 - useEffect
-  }, []) // [] 안에 counter가 들어갈시 useState가 변경될 때마다 리렌더링, []가 비어있으면 한번만 실행, 부분적으로 한번만 실행시킬때 유용
   return (
-    <div>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click Button</button>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/abot-us">
+          <h1>Hello</h1>
+        </Route>
+        <Route path="/movie/:id">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
